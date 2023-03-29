3c74b211845a152b85b8eda2846e6f6cb4321df9
Hernando Castano <hernando@hcastano.com>
2023-03-28 18:50:10 -0700

    Implement caller contract

--- caller/lib.rs
+++ caller/lib.rs
@@ -2,6 +2,14 @@
 
 #[ink::contract]
 mod caller {
+    use ink::env::{
+        call::{build_call, ExecutionInput, Selector},
+        DefaultEnvironment,
+    };
+
+    use ink::env::Result as EnvResult;
+    use ink::MessageResult;
+    type ContractResult = core::result::Result<(), ()>;
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
@@ -27,116 +35,70 @@ mod caller {
             Self::new(Default::default())
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
         /// Simply returns the current value of our `bool`.
         #[ink(message)]
         pub fn get(&self) -> bool {
             self.value
         }
-    }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+        #[ink(message)]
+        pub fn contract_error(&mut self, address: AccountId) {
+            let result = build_call::<DefaultEnvironment>()
+                .call(address)
+                .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
+                    "contract_error"
+                ))))
+                .returns::<Result<(), ()>>()
+                .try_invoke();
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let caller = Caller::default();
-            assert_eq!(caller.get(), false);
+            match result {
+                EnvResult::Ok(MessageResult::Ok(ContractResult::Err(_))) => {
+                    self.value = !self.value;
+                }
+                _ => unimplemented!(),
+            }
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut caller = Caller::new(false);
-            assert_eq!(caller.get(), false);
-            caller.flip();
-            assert_eq!(caller.get(), true);
-        }
-    }
+        #[ink(message)]
+        pub fn lang_error(&mut self, address: AccountId) {
+            let result = build_call::<DefaultEnvironment>()
+                .call(address)
+                .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
+                    "not_a_real_selector"
+                ))))
+                .returns::<()>()
+                .try_invoke();
 
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = CallerRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("caller", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<CallerRef>(contract_account_id.clone())
-                .call(|caller| caller.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
+            // Note: You can match on more specific `LangError`s here.
+            //
+            // See [`ink::LangError`] for more.
+            match result {
+                EnvResult::Ok(MessageResult::Err(_)) => {
+                    self.value = !self.value;
+                }
+                _ => unimplemented!(),
+            }
         }
 
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = CallerRef::new(false);
-            let contract_account_id = client
-                .instantiate("caller", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
+        #[ink(message)]
+        pub fn environment_error(&mut self, address: AccountId) {
+            let result = build_call::<DefaultEnvironment>()
+                .call(address)
+                .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
+                    "environment_error"
+                ))))
+                .returns::<()>()
+                .try_invoke();
 
-            let get = build_message::<CallerRef>(contract_account_id.clone())
-                .call(|caller| caller.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<CallerRef>(contract_account_id.clone())
-                .call(|caller| caller.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<CallerRef>(contract_account_id.clone())
-                .call(|caller| caller.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
+            // Note: You can match on more specific `EnvError`s here.
+            //
+            // See [`ink::env::Error`] for more.
+            match result {
+                EnvResult::Err(_) => {
+                    self.value = !self.value;
+                }
+                _ => unimplemented!()
+            }
         }
     }
 }
